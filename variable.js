5e9fa72d9915341318319620eb4c19b169966005
Faiyaz <mk3590653@gmail.com>
2025-04-29 16:56:14 +0600

    Add variable.js file

---
+++ variable.js
@@ -0,0 +1,103 @@
+// String Type Variable
+const name = 'Mohammad Faiyaz Khan';
+const address = 'Chandpur Sadar, Chandpur';
+const university_name = 'Dhaka University';
+const fan = 'tetul';
+const movieOfTheYear = 'Oshiomor';
+const hobby = 'Reading Books';
+const color = 'Black';
+
+console.log(name);
+console.log(address);
+console.log(university_name);
+console.log(fan);
+console.log(movieOfTheYear);
+console.log(hobby);
+console.log(color);
+
+//কানামাছি true, কানামাছি false
+
+let isFriendHappy = true; // boolean type variable
+console.log(isFriendHappy);
+
+let isMessageFromDear = true;
+console.log(isMessageFromDear);
+
+let celebrating = true;
+console.log(celebrating);
+
+let isHappy = true;
+let hasMoney = false;
+let isHungry = true;
+
+console.log(isHappy);
+console.log(hasMoney);
+console.log(isHungry);
+
+let isStudyDone = true;
+console.log(isStudyDone);
+
+let isPhoneCharged = false;
+console.log(isPhoneCharged);
+
+let isDinnerDone = true;
+console.log(isDinnerDone);
+
+//জাভাস্ক্রিপ্টে দিয়ে গনিতের গ্যাংস্টার
+
+let income = 100;
+let expense = 40;
+let savings = income - expense; // subtraction
+console.log(savings); // 60
+
+let havePencil = 10;
+let perUse = 2;
+let totalUseTime = havePencil / perUse; // division
+console.log(totalUseTime); // 5
+
+let initialSavings = 60;
+let perExpense = 15;
+let totalExpenseTime = initialSavings / perExpense; // division
+console.log(totalExpenseTime); // 4
+
+let hello = 'Hello';
+let world = 'World';
+let greeting = hello + '' + world; // concatenation
+console.log(greeting); // HelloWorld
+
+let rice = 153;
+let lentil = 261;
+let totalFood = rice + lentil; // addition
+console.log(totalFood); // 414
+
+let totalMoney = 500;
+let perCost = 75;
+let timeOfUse = Math.floor(totalMoney / perCost); // division
+let remainingMoney = totalMoney % perCost; // modulus
+console.log(timeOfUse);
+console.log(remainingMoney);
+
+let dividend = 8;
+let divisor = 3;
+let quotient = Math.floor(dividend / divisor); // division
+let remaider = dividend % divisor; // modulus
+console.log(quotient); // 2
+console.log(remaider); // 2
+
+let dividend1 = 50;
+let divisor1 = 9;
+let remaider1 = dividend1 % divisor1; // modulus
+console.log(remaider1); // 5
+
+let coun1 = 'Bangla';
+let coun2 = 'desh';
+let concatenation = coun1 + coun2; // concatenation
+console.log(concatenation); // Bangladesh
+
+let havingRice = 480;
+let dailyRice = 4;
+let totalDays = havingRice / dailyRice; // division
+let totalMonth = Math.floor(totalDays / 30); // division
+console.log(totalDays); // 120
+console.log(totalMonth); // 4
+
