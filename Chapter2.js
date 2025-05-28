0dc99e4d2f5e63e846fd3e0b28faebb11331a716
Faiyaz <mk3590653@gmail.com>
2025-05-28 08:29:46 +0600

    Modified Chapter2.js file

---
+++ Chapter2.js
@@ -0,0 +1,229 @@
+//Variable কি
+
+//1. তোর জন্মসালটি একটা ভেরিয়েবলে রাখ।
+const my_date_of_birth = '09 April, 2004';
+console.log(my_date_of_birth);
+
+//2. তোরা কয় ভাই-বোন, সেটা একটা ভেরিয়েবলে রাখ।
+const my_siblings = 1;
+console.log(my_siblings);
+
+//3. তোর ক্লাসে কতজন স্টুডেন্ট আছে, তাদের সংখ্যা দিয়ে students নামক একটা ভেরিয়েবল ডিক্লেয়ার কর।
+const students = 225;
+console.log(students);
+
+// 4. লাস্ট পরীক্ষায় কত নাম্বার পাইছিলি, সেটা দিয়ে marks নামক একটা ভেরিয়েবল ডিক্লেয়ার কর।
+const marks = 1055;
+console.log(marks);
+
+// 5. তোর বাসায় কয়টা রুম আছে, সেটা নিয়ে rooms নামক একটা ভেরিয়েবল ডিক্লেয়ার কর।
+const rooms = 3;
+console.log(rooms);
+
+// 6. তোর দৈনিক পড়াশোনার সময়কে ঘণ্টা হিসেবে একটি ভেরিয়েবলে রাখ।
+const study_hours_per_day = 4;
+console.log(study_hours_per_day);
+
+// 7. তোর ফোন নাম্বারে যতগুলা ডিজিট বা অঙ্ক যোগ করে যোগফল total নামে একটা ভেরিয়েবলে রাখ।
+const phone_number = '01712345678';
+const total = 0 + 1 + 7 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8;
+console.log(total);
+
+// log-এর গোল ,console
+/*
+1. তোর বর্তমান বয়স age নামে একটি ভেরিয়েবলে রাখ। কনসোলে তারপর এই ভেরিয়েবলটি প্রিন্ট কর। */
+const age = 19;
+console.log(age);
+
+/* 
+2. তুই এই বছর যে কয়টা বই পড়ার পরিকল্পনা করছস, সেটাকে booksToRead নামে একটি ভেরিয়েবলে রাখ। পরে booksToRead-এর মান কনসোলে আউটপুট দেখা। */
+const booksToRead = 12;
+console.log(booksToRead);
+
+/*
+3. তুই প্রতিদিন সকাল কয়টার সময় ঘুম থেকে উঠস, সেটাকে wakeUpTime নামে একটি ভেরিয়েবলে ডিক্লেয়ার কর। তারপর wakeUpTime-এর নতুন মান সেট কর এবং কনসোলে আউটপুট হিসেবে দেখা ৷ */
+const wakeUpTime = '6:30 AM';
+console.log(wakeUpTime);
+
+/*
+4. আজ সারাদিনে কতঘণ্টা সময় তুই মোবাইল ফোনে নষ্ট করছস, সেটাকে wastedTime নামে একটি ভেরিয়েবলে
+এই মান রাখ। তারপর এইটাকে কনসোলে দেখা। */
+const wastedTime = 3; // in hours
+console.log(wastedTime);
+
+/*
+5. এই মুহূর্তে তুই কত টাকা ধার করে রাখছস? সেটাকে borrow নামে একটি ভেরিয়েবলে রাখ। */
+const borrow = 500; // in BDT
+console.log(borrow);
+
+/*
+6. তোর প্রিয় টিম অপোজিট টিমকে সর্বোচ্চ কয়টা গোল দিছে এক ম্যাচে। সেটা দিয়ে goals নামে একটা
+ভেরিয়েবলে লিখে সেটাকে আউটপুট হিসেবে দেখা। */
+const goals = 5; // maximum goals in a match
+console.log(goals);
+
+/*
+7. তোর বিছানায় কয়টা বালিশ আছে, সেটার জন্য pillow নামে একটা ভেরিয়েবল ডিক্লেয়ার কর। তারপর কনসোলে সেটাকে আউটপুট হিসেবে দেখা।*/
+const pillow = 2;
+console.log(pillow);
+
+// String ধরে মারো টান
+
+/* 
+1. এখন name নামে একটা ভেরিয়েবল ডিক্লেয়ার কর, আর যেটার মান হবে তোর নাম। এরপর সেটাকে console - এ আউটপুট হিসেবে দেখা। */
+const name = 'Mohammad Faiyaz Khan';
+console.log(name);
+
+/* 
+2. তোর ঠিকানা (address) একটা স্ট্রিং ভেরিয়েবল ডিক্লেয়ার করে দেখ। আর সেটাকে console.log() দিয়ে আউটপুট দেখ।*/
+const address = 'Chandpur Sadar, Chandpur';
+console.log(address);
+
+/*
+3. তোর বেস্ট ফ্রেন্ড কোন স্কুল, কলেজ বা ভার্সিটিতে পড়ে, সেই স্কুল, কলেজ বা ভার্সিটির নামে একটা ভেরিয়েবল ডিক্লেয়ার কর। তারপর সেটাকে আউটপুট হিসেবে দেখা। */
+const bestFriendSchool = 'Chandpur Government High School';
+console.log(bestFriendSchool);
+
+/*
+4. তোর প্রিয় নায়িকার নাম fan নামে একটা ভেরিয়েবলে রাখ। তারপর কনসোলে দেখা ৷*/
+const fan = 'tetul';
+console.log(fan);
+
+/*
+5. এই বছরের তোর প্রিয় সিনেমার নাম movieOfTheYear নামে একটা ভেরিয়েবলে রাখ। তারপর সেটা কনসোলে
+প্রিন্ট কর। */
+const movieOfTheYear = 'Oshiomor';
+console.log(movieOfTheYear);
+
+/*
+6. সবার কিছু শখ বা hobby থাকে। তোর শখ বা hobby নামে একটা স্ট্রিং ভেরিয়েবল ডিক্লেয়ার কর। */
+const hobby = 'Reading Books';
+console.log(hobby);
+
+/*
+7. তোর প্রিয় রং কোনটা? color নামে একটা স্ট্রিং ভেরিয়েবল ডিক্লেয়ার কর। */
+const color = 'Black';
+console.log(color);
+
+//কানামাছি true, কানামাছি false
+
+/* 
+1. ধর তোর বন্ধু পরীক্ষায় বাম্বু খাইছে। এখন সে হ্যাপি কি না, সেটা একটা boolean টাইপের ভেরিয়েবল ডিক্লেয়ার করে সেটার মান রাখ। আর সেটা console-এ প্রিন্ট কর। */
+const isFriendHappy = true; // boolean type variable
+console.log(isFriendHappy);
+
+/*
+2. তুই নিজে একটা প্রোগ্রাম লিখে দেখ, তোর crush তোকে মেসেজ দিয়েছে কি না। ধর মেসেজ দিলে true, না দিলে false। সেটা প্রিন্ট কর console.log() দিয়ে। */
+
+const isMessageFromDear = true;
+console.log(isMessageFromDear);
+
+/*
+3. তোর প্রিয় টিম খেলায় জিতে গেল, এখন সেই খুশিতে বগল বাজাতে বাজাতে celebrating নামে একটা boolean ভেরিয়েবল ডিক্লেয়ার করে সেটার মান দে, আর আউটপুট দেখ। */
+const celebrating = true;
+console.log(celebrating);
+
+/*
+4. তোর এখন boolean টাইপ সম্পর্কে ভালো ধারণা হয়েছে। এবার একটা প্রোগ্রাম লিখ, যেখানে তুই তিনটা boolean ভেরিয়েবল ডিক্লেয়ার করবি: "isHappy", "hasMoney", আর "isHungry"। এই তিনটার মান যথাক্রমে true, false, আর true সেট করবি। তারপর আউটপুট দে console.log() দিয়ে। */
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+console.log(isHappy);
+console.log(hasMoney);
+console.log(isHungry);
+
+/*
+5. তোর আজকে পড়া শেষ হয়েছে কি না? পড়া শেষ হলে true, না হলে false হবে। আর সেটা রাখার জন্য isStudy Done নামে একটা boolean ভেরিয়েবল ডিক্লেয়ার কর।*/
+const isStudyDone = true;
+console.log(isStudyDone);
+
+/*
+6. এখন isPhoneCharged নামে একটা boolean ভেরিয়েবল ডিক্লেয়ার কর। চার্জ শেষ হলে true, শেষ না হলে false। */
+const isPhoneCharged = false;
+console.log(isPhoneCharged);
+
+/* 
+7. তোর কি রাতের খাওয়া শেষ? সেটা নিয়ে isDinnerDone নামে একটি boolean ভেরিয়েবল ডিক্লেয়ার কর।
+খাওয়া শেষ হলে কী লিখবি, আর শেষ না হলে কী লিখবি, সেটা তো জানোস অলরেডি। */
+const isDinnerDone = true;
+console.log(isDinnerDone);
+
+//জাভাস্ক্রিপ্টে দিয়ে গনিতের গ্যাংস্টার
+
+/*
+1. ধর, তুই 100 টাকা আয় করেছিস। তার মধ্যে 40 টাকা খরচ করছিস। বাকি টাকা কত রইল, সেটা বের করার একটা প্রোগ্রাম লিখ।*/
+const income = 100;
+const expense = 40;
+const savings = income - expense; // subtraction
+console.log(savings); // 60
+
+/*
+2. তোর কাছে 10টা পেন্সিল আছে। প্রতিবার তুই একসাথে 2টা পেন্সিল ব্যবহার করবি। কত বার ব্যবহার করতে পারবি? */
+const havePencil = 10;
+const perUse = 2;
+const totalUseTime = havePencil / perUse; // division
+console.log(totalUseTime); // 5
+
+/*
+3. প্রোগ্রাম লিখে বের কর।তোর কাছে 60 টাকা আছে। তুই যদি প্রতিবার 15 টাকা করে খরচ করিস, কয়বার খরচ করতে পারবি? এবং খরচের পর কয় টাকা বাকি থাকবে, সেটা বের করতে একটা প্রোগ্রাম লিখ।*/
+const initialSavings = 60;
+const perExpense = 15;
+const totalExpenseTime = initialSavings / perExpense; // division
+console.log(totalExpenseTime); // 4
+
+/*
+4. ধর, তুই দুইটা স্ট্রিং লিখলি "Hello" আর "World"। এই দুইটা স্ট্রিংকে একসাথে জোড়া দিয়ে কীভাবে
+"HelloWorld" বানানো যায়, সেটা দেখানোর জন্য একটা প্রোগ্রাম লিখ। */
+const Hello = 'Hello';
+const world = 'World';
+const greeting = Hello + '' + world; // concatenation
+console.log(greeting); // HelloWorld
+
+/*
+5. তুই যদি 153 কেজি চাল আর 261 কেজি ডাল কিনলি, তাহলে তুই কয় কেজি জিনিস কিনেছিস? মোট কেজি বের করার একটা প্রোগ্রাম লিখ।*/
+const rice = 153;
+const lentil = 261;
+const totalFood = rice + lentil; // addition
+console.log(totalFood); // 414
+
+/*
+6. তোর কাছে 500 টাকা আছে, প্রতিবার তুই 75 টাকা খরচ করবি। কয়বার খরচ করতে পারবি আর শেষে কয় টাকা বাকি থাকবে, সেটা বের করার একটা প্রোগ্রাম লিখ।*/
+const totalMoney = 500;
+const perCost = 75;
+const timeOfUse = Math.floor(totalMoney / perCost); // division
+const remainingMoney = totalMoney % perCost; // modulus
+console.log(timeOfUse);
+console.log(remainingMoney);
+
+/*
+7. তুই 8-কে 3 দিয়ে ভাগ দিলে ভাগফল কত আর ভাগশেষ কত, সেটা বের করার জন্য একটা প্রোগ্রাম লিখ। */
+const dividend = 8;
+const divisor = 3;
+const quotient = Math.floor(dividend / divisor); // division
+const remaider = dividend % divisor; // modulus
+console.log(quotient); // 2
+console.log(remaider); // 2
+
+/* 
+8. তুই 50-কে 9 দিয়ে ভাগ দিলে শুধু ভাগশেষ কত থাকে, সেটা বের করার একটা প্রোগ্রাম লিখ।*/
+const dividend1 = 50;
+const divisor1 = 9;
+const remaider1 = dividend1 % divisor1; // modulus
+console.log(remaider1); // 5
+
+/*
+9. তুই "Bangla" আর "desh" স্ট্রিং যোগ করে "Bangladesh" বানাতে চাস। কীভাবে করবি, সেটা দেখানোর একটা প্রোগ্রাম লিখ । */
+const coun1 = 'Bangla';
+const coun2 = 'desh';
+const concatenation = coun1 + coun2; // concatenation
+console.log(concatenation); // Bangladesh
+
+/*
+10. ধর, একদিন ঘুম থেকে উঠে ক্ষিধার চোটে তুই ছোটখাটো একটা রাক্ষস হয়ে গেছস। তারপর থেকে প্রতিদিন 4
+কেজি চালের ভাত খেয়ে ফেলস। এখন যদি তোকে 12 মন (480 কেজি) চাল দেয়া হয় থাকে, তাহলে এই চাল দিয়ে তোর কত দিন যাবে। আবার একমাস যদি 30 দিনে হয়, তাহলে এই 12 মন চাল দিয়ে তোর কত মাস যাবে। */
+const havingRice = 480;
+const dailyRice = 4;
+const totalDays = havingRice / dailyRice; // division
+const totalMonth = Math.floor(totalDays / 30); // division
+console.log(totalDays); // 120
+console.log(totalMonth); // 4
